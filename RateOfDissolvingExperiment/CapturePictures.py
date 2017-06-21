f69e14c65316d4b5b0296a25d90ad3aea493df95
laurelrwoods <laurelrwoods@gmail.com>
2017-06-21 15:15:24 -0700

    Update CapturePictures.py

--- RateOfDissolvingExperiment/CapturePictures.py
+++ RateOfDissolvingExperiment/CapturePictures.py
@@ -6,13 +6,10 @@ from botocore.client import Config
 import socket
 import os
 
-ACCESS_KEY_ID = ''
-ACCESS_SECRET_KEY = ''
+
 BUCKET_NAME = 'ice911'
 s3 = boto3.resource(
     's3',
-    aws_access_key_id=ACCESS_KEY_ID,
-    aws_secret_access_key=ACCESS_SECRET_KEY,
     config=Config(signature_version='s3v4')
 )
 
